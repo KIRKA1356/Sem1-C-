096a4d07a52392b9e71471f83680249bba17f27e
KIRKA1356 <kgritsun@mail.ru>
2022-06-19 20:56:41 +0300

    Программа проверки что первое число являеться квадратом второго числа

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿ int num = 5;
+﻿int num1, num2, quad;                       //Объявление необходиммых переменных
 
-Console.WriteLine("My number is " + num);
+Console.Write("Imput first number: ");      //Предложение ко вводу первого числа
+num1 = Convert.ToInt32(Console.ReadLine()); //Ввод первого числа с конвертацией
 
-Console.WriteLine("Imput your number!");
-num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput second number: ");     //Предложение ко вводу второго числа
+num2 = Convert.ToInt32(Console.ReadLine()); //Ввод второго числа
 
-Console.WriteLine("Your number is " + num);
+quad=num2*num2;                             //Условие
+
+if (quad==num1)                             //Условный оператор, при котором будут выодиться ответы
+{
+    Console.WriteLine("Yes");           
+}
+else
+{
+        Console.WriteLine("No");
+}
