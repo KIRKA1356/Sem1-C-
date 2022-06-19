26666f417d1cc92356fa6277dc8520bd94fea1ad
KIRKA1356 <kgritsun@mail.ru>
2022-06-19 20:44:35 +0300

    Cоздал первую прогу

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿ int num = 5;
+
+Console.WriteLine("My number is " + num);
+
+Console.WriteLine("Imput your number!");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
