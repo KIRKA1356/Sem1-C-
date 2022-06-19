0ec16d82a8167073e23f83eb2680bdc1ef16cc1b
KIRKA1356 <kgritsun@mail.ru>
2022-06-19 21:21:59 +0300

    прога выводит название дня недели по цифре

--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
-﻿int num, num2;
+﻿int n1;
 
-Console.Write("ВВедите число, квадрат которого хотите получить: ");
-num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите цифру, день недели по которой хотите получить:");
+n1 = Convert.ToInt32(Console.ReadLine());
 
-num2 = num*num;
+if (n1==1)
+{
+    Console.WriteLine("Понедельник");
+ }
+ else 
+    if (n1==2)
+    {
+        Console.WriteLine("Вторник");
+    }
+        else    
+            if (n1==3)
+            {
+                Console.WriteLine("Среда");
+            }
+                else
+                    if (n1==4)
+                    {
+                        Console.WriteLine("Четверг");
+                    }
+                        else 
+                            if (n1==5)
+                            {
+                                Console.WriteLine("Пятница");
+                            }
+                                else 
+                                    if (n1==6)
+                                    {
+                                        Console.WriteLine("Суббота");
+                                    } 
+                                    else     
+                                        if (n1==7)
+                                        {
+                                            Console.WriteLine("Воскресенье");
+                                        }
+                                            else
+                                            {
+                                                Console.WriteLine("дурак блять!!!!!");
+                                            }
 
-
-Console.Write("Ваше число в квадрате равно " + num2);
